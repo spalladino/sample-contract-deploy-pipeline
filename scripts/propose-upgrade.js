6afab715e422305b6fd1ee3ab3b38ff486ee8875
Santiago Palladino <spalladino@gmail.com>
2022-10-07 20:24:35 -0300

    Output additional upgrade info

--- scripts/propose-upgrade.js
+++ scripts/propose-upgrade.js
@@ -3,9 +3,10 @@ const { defender } = require('hardhat');
 const address = process.env.ADDRESS;
 const owner = process.env.OWNER;
 const url = process.env.WORKFLOW_URL;
+const commit = require('child_process').execSync(`/usr/bin/git log -1 --format='%H'`).toString().trim();
 
 async function main() {
-  console.log(`Upgrading box contract at ${address}`);
+  console.log(`Preparing new implementation for contract at ${address} from commit ${commit}`);
   const proposal = await defender.proposeUpgrade(address, 'Box',{ 
     bytecodeVerificationReferenceUrl: url,
     kind: 'uups',
@@ -14,7 +15,7 @@ async function main() {
     multisigType: 'EOA',
   });
   const verification = proposal.verificationResponse;
-  console.log(`Created new upgrade proposal at ${proposal.url} for artifact with digest ${verification?.providedSha256 ?? 'unknown'} (match ${verification.matchType})`);
+  console.log(`Created new upgrade proposal for contract at ${address} to implementation at ${proposal.metadata.newImplementationAddress} deployed from artifact with digest ${verification?.providedSha256 ?? 'unknown'}: ${proposal.url}`);
 }
 
 main().catch(console.error);
